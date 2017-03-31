b6d26ea76db64552bb08818c60b4edee1f88665b
Brian Plaza <brianplaza@gmail.com>
2017-03-30 21:51:36 -0400

    repo cleanup

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 		/**
 		 * Generates random particles using canvas
-		 * 
+		 *
 		 * @class Particles
 		 * @constructor
 		 */
@@ -28,7 +28,7 @@
 		  //particle speed min/max
 		  this.minSpeed = 0;
 		  this.maxSpeed = .1;
-		  //frames per second 
+		  //frames per second
 		  this.fps = 60;
 		  //number of particles
 		  this.numParticles = 750;
@@ -61,14 +61,14 @@
 		 * Sets canvas size and updates values on resize
 		 * @method render
 		 */
-		Particles.prototype.render = function(){ 
+		Particles.prototype.render = function(){
 		  var self = this,
 		      wHeight = $(window).height(),
 		      wWidth = $(window).width();
-		  
+
 		  self.canvas.width = wWidth;
 		  self.canvas.height = wHeight;
-		  
+
 		  $(window).on('resize', self.render);
 		}
 
@@ -93,7 +93,7 @@
 		      yVelocity: vy,
 		      color: 'rgba(' + color + ',' + self._rand(self.minOpacity, self.maxOpacity) + ')'
 		    }
-		    
+
 		    //once values are determined, draw particle on canvas
 		    self.draw(particle, i);
 		  }
@@ -110,38 +110,38 @@
 		Particles.prototype.draw = function(particle, i){
 		  var self = this,
 		      ctx = self.ctx;
-		  
+
 		  if (self.blurry === true ) {
 		    //creates gradient if blurry === true
 		    var grd = ctx.createRadialGradient(particle[i].xPos, particle[i].yPos, particle[i].radius, particle[i].xPos, particle[i].yPos, particle[i].radius/1.25);
-		    
+
 		    grd.addColorStop(1.000, particle[i].color);
 		    grd.addColorStop(0.000, 'rgba(34, 34, 34, 0)');
 		    ctx.fillStyle = grd;
 		  } else {
 		    //otherwise sets to solid color w/ opacity value
-		    ctx.fillStyle = particle[i].color; 
+		    ctx.fillStyle = particle[i].color;
 		  }
-		  
+
 		  if (self.border === true) {
 		        ctx.strokeStyle = '#fff';
 		        ctx.stroke();
 		  }
-		  
+
 		  ctx.beginPath();
 		  ctx.arc(particle[i].xPos, particle[i].yPos, particle[i].radius, 0, 2 * Math.PI, false);
 		  ctx.fill();
 		}
 
 		/**
-		 * Animates particles 
+		 * Animates particles
 		 * @param  {array} particle value from createCircle & draw methods
 		 * @method animate
 		 */
 		Particles.prototype.animate = function(particle){
 		  var self = this,
 		          ctx = self.ctx;
-		  
+
 		  setInterval(function(){
 		    //clears canvas
 		    self.clearCanvas();
@@ -149,15 +149,15 @@
 		    for (var i = 0; i < self.numParticles; i++) {
 		      particle[i].xPos += particle[i].xVelocity;
 		      particle[i].yPos -= particle[i].yVelocity;
-		     
+
 		      //if particle goes off screen call reset method to place it offscreen to the left/bottom
 		      if (particle[i].xPos > self.canvas.width + particle[i].radius || particle[i].yPos > self.canvas.height + particle[i].radius) {
 		        self.resetParticle(particle, i);
 		      } else {
 		        self.draw(particle, i);
 		      }
-		    }  
-		  }, 1000/self.fps); 
+		    }
+		  }, 1000/self.fps);
 		}
 
 		/**
@@ -168,17 +168,17 @@
 		 */
 		Particles.prototype.resetParticle = function(particle, i){
 		  var self = this;
-		  
+
 		  var random = self._rand(0, 1);
-		  
-		  if (random > .5) { 
+
+		  if (random > .5) {
 		    // 50% chance particle comes from left side of window...
 		    particle[i].  xPos = -particle[i].radius;
 		        particle[i].yPos =   self._rand(0, canvas.height);
 		  } else {
 		    //... or bottom of window
 		    particle[i].  xPos = self._rand(0, canvas.width);
-		        particle[i].yPos =   canvas.height + particle[i].radius;   
+		        particle[i].yPos =   canvas.height + particle[i].radius;
 		  }
 		  //redraw particle with new values
 		  self.draw(particle, i);
@@ -192,10 +192,10 @@
 		  this.ctx.clearRect(0, 0, canvas.width, canvas.height);
 		}
 
-		 
+
 		// go go go!
 		var particle = new Particles();
-		particle.init(); 
+		particle.init();
 
 	});
 
