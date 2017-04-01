5f9d1e3a8b82e03916bd4df6d371248d2647aff5
Brian Plaza <brianplaza@gmail.com>
2017-03-31 23:58:59 -0400

    refactored folder structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('css'))
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'));
 })
 
 gulp.task('watch', function(){
-  gulp.watch('scss/**/*.scss', ['sass']);
-  // Other watchers
+  gulp.watch('app/scss/**/*.scss', ['sass']);
 })
 
 gulp.task('default', ['sass', 'watch']);
